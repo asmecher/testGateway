6b82c5ea93c0223a1da0b1e0a06f2a8bef1b9089
Alec Smecher <alec@smecher.bc.ca>
2015-02-20 16:55:41 -0800

    Add functional test

---
+++ tests/functional/TestGatewayPluginTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file tests/functional/TestGatewayFunctionalTest.php
+ *
+ * Copyright (c) 2014-2015 Simon Fraser University Library
+ * Copyright (c) 2000-2015 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @class TestGatewayFunctionalTest
+ * @package plugins.gateways.testGateway
+ *
+ * @brief Functional tests for the test gateway plugin.
+ */
+
+import('lib.pkp.tests.WebTestCase');
+
+class TestGatewayFunctionalTest extends WebTestCase {
+	/**
+	 * @copydoc WebTestCase::getAffectedTables
+	 */
+	protected function getAffectedTables() {
+		return PKP_TEST_ENTIRE_DB;
+	}
+
+	/**
+	 * Enable the plugin
+	 */
+	function testTestGateway() {
+		$this->open(self::$baseUrl);
+
+		// Check that the gateway is not serving.
+		$this->open(self::$baseUrl . '/index.php/publicknowledge/gateway/plugin/TestGatewayPlugin');
+		$this->assertTextNotPresent('This request is being served by the test gateway plugin.');
+
+		$this->logIn('admin', 'admin');
+		$this->waitForElementPresent($selector='link=Website');
+		$this->clickAndWait($selector);
+		$this->click('link=Plugins');
+
+		// Find and enable the plugin
+		$this->waitForElementPresent($selector = '//input[@id=\'select-cell-TestGatewayPlugin-enabled\']');
+		$this->click($selector); // Enable plugin
+		$this->waitForElementPresent('//div[contains(text(),\'The plugin "Test Gateway Plugin" has been enabled.\')]');
+
+		// Check that the gateway is not serving.
+		$this->open(self::$baseUrl . '/index.php/publicknowledge/gateway/plugin/TestGatewayPlugin');
+		$this->assertTextPresent('This request is being served by the test gateway plugin.');
+
+		// Find the plugin's tab
+		$this->open(self::$baseUrl);
+		$this->logOut();
+	}
+}
